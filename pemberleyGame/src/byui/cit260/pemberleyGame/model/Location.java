33932597a59285b284bfe4d980f153af59682cc8
Cody <Cody@Marriott-PC.Home>
2016-01-28 20:29:34 -0800

    Location Class

---
+++ pemberleyGame/src/byui/cit260/pemberleyGame/model/Location.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pemberleyGame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author MMarriott
+ */
+public class Location implements Serializable{
+
+    //class instance variables
+    private String name;
+    private String description;
+    
+    
+    public Location() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "name=" + name + ", description=" + description + '}';
+    }
+    
+    
+    
+}
