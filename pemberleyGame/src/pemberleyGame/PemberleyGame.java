ea6850b823abfd003514ba89d90ea7db51d61c3a
wilkerson-sheila <swinaz99@yahoo.com>
2016-01-30 14:10:12 -0700

    Update PemberleyGame.java

--- pemberleyGame/src/pemberleyGame/PemberleyGame.java
+++ pemberleyGame/src/pemberleyGame/PemberleyGame.java
@@ -1,7 +1,7 @@
 /*
  * Test files - instances of the classes
  */
-package PemberleyGame;
+package pemberleyGame;
 
 import byui.cit260.pemberleyGame.model.Location;
 import byui.cit260.pemberleyGame.model.Player;
