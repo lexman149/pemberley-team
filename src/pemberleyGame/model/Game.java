a7912d431a0e47d9fef81144baa400838057ae31
mbp <mbp@192.168.21.132>
2016-01-30 00:53:05 -0700

    1/27/16 Class files to repository (L03 individual assignment)

---
+++ src/pemberleyGame/model/Game.java
@@ -0,0 +1,55 @@
+package pemberleyGame.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author wilkerson-sheila
+ */
+public class Game implements Serializable{
+    
+    // class instance variables //
+    private int timeRemaining;
+    
+    // constructor function //
+    public Game() {
+    }
+    
+   // getters & setters //
+    public int getTimeRemaining() {
+        return timeRemaining;
+    }
+
+    public void setTimeRemaining(int timeRemaining) {
+        this.timeRemaining = timeRemaining;
+    }
+    // string, equals & hash //
+
+    @Override
+    public String toString() {
+        return "Game{" + "Time Remaining = " + timeRemaining + " seconds" + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 37 * hash + this.timeRemaining;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.timeRemaining != other.timeRemaining) {
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ src/pemberleyGame/model/Map.java
@@ -0,0 +1,71 @@
+package pemberleyGame.model;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author wilkerson-sheila
+ */
+public class Map implements Serializable{
+    
+     //class instance variables //
+    private String name;
+    private String room;
+    
+    
+    // constructor function //
+    public Map() {
+    }
+
+    // getters & setters // 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRooms() {
+        return room;
+    }
+
+    public void setRooms(String room) {
+        this.room = room;
+    }
+    
+    // string, equals & hash //
+
+    @Override
+    public String toString() {
+        return "Map{" + "Map Name = " + name + ", Room = " + room + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + Objects.hashCode(this.name);
+        hash = 23 * hash + Objects.hashCode(this.room);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Map other = (Map) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.room, other.room)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
+
