3e42337dde09f9a0d0740a70fc459a5c7de44f9b
jpmanning <jpmanning@192.168.2.3>
2016-02-23 22:07:50 -0800

    Merge origin/master
    Conflicts:
    	pemberleyGame/src/pemberleyGame/PemberleyGame.java
    edited: Lexi 2.23.16
    edited: Lexi 2.23.16 w/Melissa

--- pemberleyGame/src/byui/cit260/pemberleyGame/control/MapControl.java
+++ pemberleyGame/src/byui/cit260/pemberleyGame/control/MapControl.java
@@ -5,10 +5,52 @@
  */
 package byui.cit260.pemberleyGame.control;
 
+import byui.cit260.pemberleyGame.model.*;
+
 /**
  *
  * @author mbp
  */
 public class MapControl {
+    
+    public String playerNavigate(String direction, Player player) {
+        Room currentRoom = player.getLocation();
+        Room nextRoom;
+        direction = direction.toUpperCase();
+        direction = direction.trim();
+        System.out.println(direction);
+        switch (direction) { 
+        case"E":
+        case"EAST":    
+            nextRoom = currentRoom.getEast(); 
+            break;
+        case"S":
+        case"South":
+            nextRoom = currentRoom.getSouth(); 
+            break;
+        case"N":
+        case"North":
+            nextRoom = currentRoom.getNorth(); 
+            break;
+        case"W":
+        case"West":
+            nextRoom = currentRoom.getWest(); 
+            break;
+            
+        default: 
+            return "Not sure where you are going.";
+            
+        } 
+        
+        
+        
+        if (nextRoom == null) {
+            return "You cannot go that direction. Please choose again.";
+        }
+        else {
+            player.setLocation(nextRoom);
+            return nextRoom.getDescription();
+        }
+    }
     
 }
