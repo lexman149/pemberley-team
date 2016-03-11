bad574b68ae4fea424cd25fb74880c169cec9555
mbp <mbp@192.168.21.132>
2016-03-10 19:15:18 -0700

    3/10/16 created MapCoordinates Enum

---
+++ pemberleyGame/src/byui/cit260/pemberleyGame/model/MapCoordinates.java
@@ -0,0 +1,66 @@
+package byui.cit260.pemberleyGame.model;
+
+import java.io.Serializable;
+
+public enum MapCoordinates implements Serializable{
+        mpLampton(new int [][]{
+            {0, -1, -1}, // miliner
+            {1, 2, -1},	// road west, road East
+            {3, 4, 5}}), // grocer, smithy, green
+       
+
+	mpPemberleyDownstairs(new int [][]{
+            {-1, 0, -1}, // null, Cellar, null
+            {1, 2, 3}, // back stairs, kitchen, downstairs hall
+            {-1, 4, 5}}),  // null, Larder, Servants
+        
+    
+    mpPemberleyMain(new int [][]{
+        {0, 1, 2, -1}, // music room, dining room, library, null
+        {3, 4, 5, -1}, // ballroom, Great Hall, drawing room, null
+        {-1, 9, 10, 11}}), // null, front drive, path, lake
+
+    
+    mpPemberleyUpstairs(new int [][]{
+        {0, 1, -1}, // gallery, Rose's Room, null
+        {2, 3, 4}, // up hall, up hall, Mr Ws
+        {5, -1, -1}}), // grand staircase, null, null
+
+    
+    mpFinnHouse(new int [][]{
+        {0, -1}, // kitchen, null
+        {1, 2}, // living room, closet
+        {3, -1}}); // secret lab, null
+    
+    private final int [][] roomPositions;
+
+    private MapCoordinates(int[][] roomPositions) {
+        this.roomPositions = roomPositions;
+    }
+
+    public static MapCoordinates getMpLampton() {
+        return mpLampton;
+    }
+
+    public static MapCoordinates getMpPemberleyDownstairs() {
+        return mpPemberleyDownstairs;
+    }
+
+    public static MapCoordinates getMpPemberleyMain() {
+        return mpPemberleyMain;
+    }
+
+    public static MapCoordinates getMpPemberleyUpstairs() {
+        return mpPemberleyUpstairs;
+    }
+
+    public static MapCoordinates getMpFinnHouse() {
+        return mpFinnHouse;
+    }
+
+    public int[][] getRoomPositions() {
+        return roomPositions;
+    }
+    
+    
+}
