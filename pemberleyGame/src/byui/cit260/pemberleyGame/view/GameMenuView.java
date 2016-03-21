e5f67a8101921a0a7f6a3fa83cb9c09ade483a23
jpmanning <jpmanning@192.168.2.3>
2016-03-21 09:32:39 -0700

    Merge origin/master Lexi 3.21.16
    parse with Melissa inventoryNames

--- pemberleyGame/src/byui/cit260/pemberleyGame/view/GameMenuView.java
+++ pemberleyGame/src/byui/cit260/pemberleyGame/view/GameMenuView.java
@@ -11,6 +11,9 @@ import byui.cit260.pemberleyGame.model.Map;
 import byui.cit260.pemberleyGame.model.Player;
 import byui.cit260.pemberleyGame.model.Room;
 import byui.cit260.pemberleyGame.control.*;
+import byui.cit260.pemberleyGame.model.Inventory;
+import byui.cit260.pemberleyGame.model.Item;
+import java.util.Scanner;
 import pemberley_game.PemberleyGame;
 
 /**
@@ -69,8 +72,31 @@ public class GameMenuView extends View {
     }
 
     private void displayInventory() {
-        System.out.println("*** displayInventory function called ***");
-    }
+      Game game = PemberleyGame.getCurrentGame();
+      
+      int itemSelection = 0;
+      String playerSelection = " ";
+    
+      Item[] inventoryNames = game.getInventoryItemArray();
+      for (int i = 0; i < inventoryNames.length; i++) {
+      System.out.println(i + " " + inventoryNames[i].getName()); 
+  }
+
+      String prompt = "what do you want to look at or X to exit"; 
+         do {
+            playerSelection = this.getStringInput(prompt);
+
+         try {
+                    itemSelection = Integer.parseInt(playerSelection); // converts string to int
+                    System.out.println(inventoryNames[itemSelection].getDescription());
+                } catch (NumberFormatException nf) {
+                    System.out.println("\nYou must enter a valid number."
+                            + " Try again or type X to exit.");
+                    break;
+                }
+        } while (!playerSelection.equalsIgnoreCase("x"));
+
+}
 
     private void displayMap() {
         Game game = PemberleyGame.getCurrentGame();
@@ -95,6 +121,32 @@ public class GameMenuView extends View {
 
     private void displayCurrentTasks() {
         System.out.println("*** displayCurrentTasks function called ***");
+    }
+    
+    private String getStringInput(String prompt) {
+        boolean valid = false; // indicates if the name has be retrieved
+        String playersInput = null;
+        Scanner keyboard = new Scanner(System.in); // keyboard input stream
+
+        while (!valid) { // while a valid name has not been retrieved
+
+// prompt for the player's choice
+            System.out.println(prompt);
+
+// get the selection from the keyboard and trim off the blanks CAPs ok
+            playersInput = keyboard.nextLine();
+            playersInput = playersInput.trim();
+            playersInput = playersInput.toUpperCase();
+
+// if the selection is invlaid (less than one characters in length)
+            if (playersInput.length() < 1) {
+                System.out.println("Invalid selection - selection can not be blank");
+                continue; // and repeat again 
+            }
+            break; // out of the (exit) the repetition
+        }
+
+        return playersInput; // return the name
     }
 
 }
