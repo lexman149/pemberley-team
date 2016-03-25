f3d843643db947cf3ded498cde9377652389c83c
jpmanning <jpmanning@192.168.2.3>
2016-03-25 12:44:19 -0700

    Merge origin/master Lexi 3.25.16
    writeItems...

--- pemberleyGame/src/byui/cit260/pemberleyGame/view/ReportMenuView.java
+++ pemberleyGame/src/byui/cit260/pemberleyGame/view/ReportMenuView.java
@@ -89,6 +89,7 @@ public class ReportMenuView extends View{
                             + fileName);     
         }while (!fileName.equalsIgnoreCase("x"));
     }
+   
      
      
     private void saveActorLocationReport(String fileName) {  
@@ -140,6 +141,80 @@ public class ReportMenuView extends View{
         
         }
     }
+    
+    
+    /*private void writeItemLocationReport() {
+// 3a
+        this.console.println("\n\nEnter a file name where this report will "
+                            + "be saved. Type X to exit");
+ // 3b  
+        String fileName = this.getInput();
+        do{
+         // getInput function in View.java
+           if (item.getLocation()!= null) { //otherwise print locationName
+             outFile.write(item.getName() + "\t" + item.getLocation().getName() + "\n");
+        }
+        else { 
+          outFile.write(item.getName() + "\t" + location + "\n");
+        }
+        }
+ // 3c
+        try {
+            // save the game to the specified file
+            saveItemLocationReport(fileName);
+        } catch (Exception ex) {
+            ErrorView.display("ERROR", ex.getMessage());
+        }
+ // 3d
+        this.console.println("The Item Location Report was saved to\n "
+                            + fileName);     
+        }while (!fileName.equalsIgnoreCase("x"));
+    }
+     
+     
+    private void saveItemLocationReport(String fileName) {  
+        
+        Game game = PemberleyGame.getCurrentGame(); // access to data
+        Item[] allItemArray = game.getAllItemArray(); // form with the item's info
+        try (FileWriter outFile = new FileWriter(fileName)) { // try-with-resources 
+            
+// make headers for the report.
+            outFile.write("\n\n          ITEM LOCATION REPORT          ");        
+            outFile.write("\nItem Name                     Location");
+            outFile.write("\n----------                     --------");
+
+// print item name and location
+            for(Item item : allItemArray){ // goes through all the items
+                // null check - won't call a locationName if = null
+                String location = "not applicable"; // if location is null, print this
+                if (item.getLocation() != null){ // otherwise print locationName
+                    location = item.getLocation().getName();
+                }
+                else {
+                outFile.write(item.getName() + "\t" + item.getLocation().getName() + "\n");
+                } 
+            }
+            
+                 try (PrintWriter out = new PrintWriter(fileName)) { // try-with-resources 
+//            
+//// make headers for the report.
+//            out.println("\n\n          ITEM LOCATION REPORT          ");        
+//            out.printf("%n%-30s%14s", "Item Name", "Location");
+//            out.printf("%n%-30s%14s", "----------", "--------");
+//
+//// print item name and location
+//            for(Item item : allItemArray){ // goes through all the items
+//                // null check - won't call a locationName if = null
+//                String location = "not applicable"; // if location is null, print this
+//                if (item.getLocation() != null){ // otherwise print locationName
+//                    location = item.getLocation().getName();
+//                }
+//                else {
+//                out.printf("%n%-30s%14s", item.getName()
+//                                        , item.getLocation().getName());
+//                } 
+//            }
+*/
     
    
    
