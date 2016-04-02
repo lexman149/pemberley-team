6d8866641ca33ed31823638f2f63259964919d2d
Cody <Cody@192.168.0.5>
2016-04-01 21:17:59 -0700

    added some quest info

--- pemberleyGame/src/byui/cit260/pemberleyGame/control/GameControl.java
+++ pemberleyGame/src/byui/cit260/pemberleyGame/control/GameControl.java
@@ -299,7 +299,7 @@ public class GameControl {
         atWilliam.setAlternateLocation(rmMilliner);
         atWilliam.setAlternateDescription("William the Stablehand appears to be looking"
                                         + "\nat a pretty little hat.  Is it for a girl"
-                                        + "\nhe likes.");
+                                        + "\nhe likes?");
         atWilliam.setCharacterScript("William says \"I don't have time to get the coach ready.  These horses are being difficult.\"");
         atWilliam.setDescription("William the Stable hand appears to be having some trouble with the horses  He seems a little distracted himself.");
         atWilliam.setLocation(rmStable);
